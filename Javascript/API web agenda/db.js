701bfbcce7e36618a6c987517787867e6f155b3a
Felipe Teixeira <felipeteixeira.tech>
2026-07-27 20:19:53 -0300

    primeira API

---
+++ Javascript/API web agenda/db.js
@@ -0,0 +1,18 @@
+
+const agenda = [{
+    id: 1,
+    nome: 'Adamastor da Silva',
+    telefone: '42999999999',
+    email: 'adamastor@gmail.com',
+    nota: 'Gosta de jogar truco',
+    ativo: true
+}, {
+    id: 2,
+    nome: 'Gervasio da Silva',
+    telefone: '42999999988',
+    email: 'gerva@gmail.com',
+    nota: 'Gosta de jogar peteca',
+    ativo: true
+}];
+
+export default agenda;
---
+++ Javascript/API web agenda/server.js
@@ -0,0 +1,6 @@
+import agenda from "./db";
+import express from "express";
+
+const app = express();
+
+app.listen(300, () =>console.log('AGENDA - API WEB executando...'));
